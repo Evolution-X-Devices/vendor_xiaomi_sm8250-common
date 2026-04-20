871253e3844426f3a3edd2ec6525a6261c04a621
TechPanelGM <giomook2003@gmail.com>
2026-04-20 14:01:01 +0000

    sm8250-common: Import swdap from HyperOS

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -245,6 +245,7 @@ PRODUCT_PACKAGES += \
     libhwdap \
     libmisoundfx \
     libshoebox \
+    libswdap \
     libswgamedap \
     libswvqe \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
